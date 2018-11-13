9281f9fd7f1332ba7751773643b06718161079e2
kozubenko <andrii.kozubenko@gmail.com>
2018-11-13 23:08:26 +0200

    [T-3] add test for sagas, complete basic tutorial

--- sagas.js
+++ sagas.js
@@ -1,12 +1,12 @@
 import { delay } from 'redux-saga'
-import { put, takeEvery, all } from 'redux-saga/effects'
+import {put, takeEvery, all, call} from 'redux-saga/effects'
 
 function* helloSaga() {
     console.log('Hello Sagas!')
 }
 
-function* incrementAsync() {
-    yield delay(1000)
+export function* incrementAsync() {
+    yield call(delay, 1000)
     yield put({type: 'INCREMENT'})
 }
 
